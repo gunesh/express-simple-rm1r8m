1a1ea4c2864594684ac5d437c6d564bc7b74832a
gunesh <gunesh26@gmail.com>
2022-08-27 22:54:33 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3010;
 const path = require('path');
+const ytdl = require('ytdl-core');
+app.set('view engine', 'ejs');
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve('pages/index.html'));
+  return res.render('index');
 });
 
 app.listen(port, () => {
